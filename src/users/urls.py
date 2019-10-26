13c5bca51a7b673cd8615b9e1f466a1be0f16955
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2019-10-26 21:57:29 +0530

    fixed data tables rendering issue, added css, js: DTL

--- src/users/urls.py
+++ src/users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
    	# Login
     re_path(r'^login/$', views.LoginView.as_view(), name="login_url"),
     re_path(r'^register/$', views.RegisterView.as_view(), name="register_url"),
-    re_path(r'^forgot_password/$', views.ForgotPasswordView.as_view(), name="forgot_password_url"),
+    re_path(r'^forgot-password/$', views.ForgotPasswordView.as_view(), name="forgot_password_url"),
     re_path(r'^logout/$', views.LogoutView.as_view(), name="logout_url"),
 
     re_path(r'^charts/$', views.ChartsView.as_view(), name="charts_url"),
