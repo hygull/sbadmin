a144c2987f5707913ff926dc4928d3e09e0df6e7
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2019-10-26 23:40:15 +0530

    updated title in all pages

--- src/sbadmin/context_processors/website_configuration_parameters.py
+++ src/sbadmin/context_processors/website_configuration_parameters.py
@@ -6,7 +6,7 @@ def website_configuration_parameters(request):
 	conf_params = {
 		"WEBSITE_FOOTER_NAME": WEBSITE_FOOTER_NAME,
 		"WEBSITE_TOP_LEFT_NAME": WEBSITE_TOP_LEFT_NAME,
-		"CURRENT_YEAR": datetime.now().year + 1
+		"CURRENT_YEAR": datetime.now().year
 	}
 
 	return conf_params
